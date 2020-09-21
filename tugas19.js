9a9c090f571e981d12f2b4a223d709a6b2fc4edc
hantriseptiania <71365751+hantriseptiania@users.noreply.github.com>
2020-09-21 11:43:16 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,37 @@
+var ulang ="Semangat..."
+var hasil2 = ulang.repeat(5)
+
+console.log(hasil2)
+
+var ulang ="*****"
+console.log(ulang)
+var hasil1 = ulang.repeat(4)
+
+console.log(hasil1)
+
+var hasil2 = ulang.repeat(8)
+
+console.log(hasil2)
+
+var hasil3 = ulang.repeat(12)
+
+console.log(hasil3)
+
+var hasil4 = ulang.repeat(16)
+
+console.log(hasil4)
+
+var hasil5 = ulang.repeat(12)
+
+console.log(hasil5)
+
+var hasil6= ulang.repeat(8)
+
+console.log(hasil6)
+
+var hasil7= ulang.repeat(4)
+
+console.log(hasil7)
+
+var ulang ="*****"
+console.log(ulang)
